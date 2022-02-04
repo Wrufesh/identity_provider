2824a662654b17643a75fd5a191d1e2bde63fc8e
Rupesh Singh <wrufesh@gmail.com>
2022-02-04 14:45:13 +0545

    create requirements file and create Oauth2 data model

--- identity_and_roles/models.py
+++ identity_and_roles/models.py
@@ -10,12 +10,36 @@ class User(Document):
     email = fields.EmailField(required=True, unique=True)
     password = fields.StringField(required=True)
 
+GRANT_TYPE_CHOICES = [('authorization_code', 'Authorization code')]
+RESPONSE_TYPE_CHOICES = [('code', 'Authorization code')]
+
 class Client(Document):
-    key = fields.StringField(required=True)
-    secret = fields.StringField(required=True)
-    public_key = fields.StringField(required=True)
-    user = fields.ReferenceField(User, reverse_delete_rule=CASCADE)
-    realms = fields.StringField(required=True)
-    default_realms = fields.StringField(required=True)
+    user = fields.ReferenceField(User)
+    grant_type = fields.StringField(max_length=128, choices=GRANT_TYPE_CHOICES)
+    response_type = fields.StringField(max_length=128, choices=RESPONSE_TYPE_CHOICES)
+    scopes = fields.StringField()
+    default_scopes = fields.StringField()
     redirect_uris = fields.StringField(required=True)
     default_redirect_uris = fields.StringField(required=True)
+
+class BearerToken(Document):
+    client = fields.ReferenceField(Client)
+    user = fields.ReferenceField(User)
+    scopes = fields.StringField()
+    access_token = fields.StringField()
+    refresh_token = fields.StringField()
+    expires_at = fields.DateTimeField()
+
+class AuthorizationCode(Document):
+    client = fields.ReferenceField(Client)
+    user = fields.ReferenceField(User)
+    scopes = fields.StringField()
+    redirect_uri = fields.StringField()
+    code = fields.StringField()
+    expiration_time = fields.DateTimeField()
+    challenge = fields.StringField()
+    challenge_method = fields.StringField()
+
+
+
+
